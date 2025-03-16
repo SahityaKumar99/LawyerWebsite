586ff300b2d7abcf536bed37f6593b7b98d6346b
SahityaKumar99 <sahityakumarbksc2018@gmail.com>
2025-03-16 16:26:55 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Intersection Observer for fade-in effect
+const sections = document.querySelectorAll('section');
+
+const options = {
+    rootMargin: '0px',
+    threshold: 0.1
+};
+
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('fade-in');
+            observer.unobserve(entry.target);
+        }
+    });
+}, options);
+
+sections.forEach(section => {
+     observer.observe(section);
+});
